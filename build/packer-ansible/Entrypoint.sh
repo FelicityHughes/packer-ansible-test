64f1acabe0bdcab61b41645676baba4562c75bb3
Felicity Hughes <Felicity.Hughes@intelematics.com>
2018-11-26 13:17:50 +1100

    Corrected brackets for debug if statement.

--- build/packer-ansible/Entrypoint.sh
+++ build/packer-ansible/Entrypoint.sh
@@ -6,7 +6,7 @@ VALID="${?}"
 if (( VALID == 0 )); then
   # If user doesn't want to run in debug mode, execute Packer script now.
   CLEAN_DBG_FLAG="$("echo" "${PACKER_DEBUG}" | "tr" "[:upper:]" "[:lower:]")"
-  if [ "${CLEAN_DBG_FLAG}" != "true" ]; then
+  if [[ "${CLEAN_DBG_FLAG}" != "true" ]]; then
     packer build "${PACKER_SCRIPT}"
   fi
 fi
